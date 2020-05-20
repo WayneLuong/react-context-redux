fce403ea142c0328e1648ce545608def320c1bc6
wayneluong <wayneluong93@gmail.com>
2020-05-20 03:05:24 +0100

    router and sass

--- routes-sass/src/Components/UserDetail.js
+++ routes-sass/src/Components/UserDetail.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams } from 'react-router-dom'
+import { useParams, useRouteMatch, Link, Route } from 'react-router-dom'
 
 export default function UserDetail({ match }) {
     //Gets the :id from the url
@@ -7,9 +7,20 @@ export default function UserDetail({ match }) {
     console.log(match.id) */
 
     let { id } = useParams()
+
+    // NESTING: The `path` lets us build <Route> paths that are
+    // relative to the parent route, while the `url` lets
+    // us build relative links.
+    const { path, url } = useRouteMatch();
+    console.log('Path: ', path, 'URL: ', url)
+
     return (
         <div>
-            Users Detail {id}
+            <h1>Users Detail {id}</h1>
+            <Link to={`${url}`}>Props v. State</Link>
+            <Route path={`${path}/:id`}>
+                {/* Go to a nested component */}
+            </Route>
         </div>
     )
 }
